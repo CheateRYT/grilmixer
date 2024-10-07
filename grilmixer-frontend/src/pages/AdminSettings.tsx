248fa02a12d0c5de72953e5b42eb1139286a582f
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-10-07 19:27:10 +0300

    feat: add button to leave from adm-pan

--- grilmixer-frontend/src/pages/AdminSettings.tsx
+++ grilmixer-frontend/src/pages/AdminSettings.tsx
@@ -53,6 +53,10 @@ const AdminSettings: React.FC = () => {
 			setErrorMessage('Ошибка при создании пароля')
 		}
 	}
+	const handleClickLeave = () => {
+		Cookies.set('admin-token', '')
+		window.location.reload()
+	}
 
 	return (
 		<div className='p-4 bg-slate-700'>
@@ -100,11 +104,17 @@ const AdminSettings: React.FC = () => {
 				</label>
 				<button
 					type='submit'
-					className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2'
+					className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 mb-3'
 				>
-					Изменитьь пароль
+					Изменить пароль
 				</button>
 			</form>
+			<button
+				onClick={handleClickLeave}
+				className=' bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mt-2 rounded'
+			>
+				Выйти из администраторской панели
+			</button>
 		</div>
 	)
 }
