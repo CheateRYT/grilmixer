8241a4646c2742c55afd821c842d3f1811ec7529
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-11-19 16:56:04 +0300

    feat: add certificates

--- grilmixer-server/src/main.ts
+++ grilmixer-server/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core'
+import * as fs from 'fs' // Импортируем модуль fs
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const httpsOptions = {
+		key: fs.readFileSync('./serts/privkey.pem'),
+		cert: fs.readFileSync('./secrets/fullchain.pem')
+	}
+	const app = await NestFactory.create(AppModule, { httpsOptions })
 	app.setGlobalPrefix('api')
 	app.enableCors()
-
 	await app.listen(4200)
 }
-
 bootstrap()
